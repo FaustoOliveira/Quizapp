3b0a4bb02d05e14544c18365ceb7befb01cc2031
Fausto Oliveira <105312418+FaustoOliveira@users.noreply.github.com>
2022-11-27 12:37:43 +0100

    Added headline

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+  {
+    "question": "Wer hat HTML erfunden",
+    "answer_1": "Robbie Williams",
+    "answer_2": "Lady  Gaga",
+    "answer_3": "Tim Berners-Lee",
+    "answer_4": "Justin Bieber",
+    "right_answer": 3
+  },
+
+  
+    
+  {
+    "question": "Welches Attribut kann NICHT für Textarea verwenden?",
+    "answer_1": "readyonly",
+    "answer_2": "max",
+    "answer_3": "from",
+    "answer_4": "spellcheck",
+    "right_answer": 1
+
+
+  },
+
+
+  {
+  "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem attribut tittle aus?",
+  "answer_1": "a[title]{...}",
+  "answer_2": "a > title {...}",
+  "answer_3": "a.title{...}",
+  "answer_4": "a=tittle {...}", 
+  },
+
+  {
+  "question": "Wie definiert man in JavaScript eine variable?",
+  "answer_1":"let 100 = rate;",
+  "answer_2": "100 = let rate;",
+  "answer_3": "rate = 100;",
+  "answer_4": "let rate = 100;",
+  "right_answer": 4
+  }
+
+];
